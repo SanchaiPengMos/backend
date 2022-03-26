c5bc5b16d08e5078c0267881820a3ede40b67123
sancahi <sanchaipangboot@gmail.com>
2022-03-27 03:30:45 +0700

    vaiidatetoken

---
+++ validateToken/validatetoken.go
@@ -0,0 +1,35 @@
+package validatetoken
+
+import (
+	"fmt"
+	"server/key"
+	"server/models/aut"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func ValidateToken(token string) (aut.JwtClaims, error) {
+
+	t, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+
+		if _, isvalid := token.Method.(*jwt.SigningMethodHMAC); !isvalid {
+			return nil, fmt.Errorf("Invalid token %s", token.Header["alg"])
+
+		}
+		return []byte(key.TOKEN_SECRET), nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var result aut.JwtClaims
+	if claims, ok := t.Claims.(jwt.MapClaims); ok && t.Valid {
+		result.Id = int(claims["id"].(float64))
+		
+		return result, nil
+
+	} else {
+		return result, err
+	}
+
+}
